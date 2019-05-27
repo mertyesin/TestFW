fb221a75a485f1568d6c622676149267e9ebfb21
Mert Yesin <mert.yesin@accenture.com>
2019-05-27 14:21:28 +0300

    chrome web driver added

--- test.py
+++ test.py
@@ -1,4 +1,11 @@
+#!/usr/bin/env python
 import pytest
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+def test_google():
+	driver = webdriver.Chrome()
+	driver.get("http://demo.guru99.com/test/newtours/index.php")
 
 def inc(x):
     return x + 1
