e19b44d6407066b5729858839d331dfdc6e3273d
mertyesin <mert.yesin@accenture.com>
2019-05-27 15:09:45 +0300

    basic selenium test added

--- test.py
+++ test.py
@@ -5,9 +5,12 @@ from selenium import webdriver
 def test_google():
 	driver = webdriver.Chrome()
 	driver.get("http://demo.guru99.com/test/newtours/index.php")
-
-def inc(x):
-    return x + 1
-
-def test_answer():
-    assert inc(3) == 4
+	
+	home_page_elements = driver.find_elements_by_xpath("/html/body/div[2]/table/tbody/tr/td[2]/table/tbody/tr[2]/td/table/tbody/tr/td[1]/a")
+	if len(home_page_elements) > 0:
+		text = home_page_elements[0].click()
+	
+	sign_on_elements = driver.find_elements_by_xpath("/html/body/div[2]/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr/td[2]/table/tbody/tr[3]/td/p/font/b")
+	if len(sign_on_elements) > 0:
+		text = sign_on_elements[0].text
+		assert text == "Welcome back to Mercury Tours!"
