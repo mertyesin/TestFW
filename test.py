fc8608cfd36616707fbae946e6dc137af6611849
mertyesin <mert.yesin@accenture.com>
2019-07-30 13:41:15 +0300

    test data file changed

--- test.py
+++ test.py
@@ -7,15 +7,18 @@ from library import *
 
 # @pytest.mark.skip(reason="no way of currently testing this")
 def test_guru98():
-    browser = Browser("chrome")
-    browser.go_to_url("http://demo.guru99.com/test/newtours/index.php")
+    with open("/home/mert/Documents/Training/TestFW/library/web_gui/data/test_data.json", "r") as jsonFile:
+        test_data = json.loads(jsonFile.read())
 
-    with open("/home/mert/Documents/Training/TestFW/library/web_gui/user_data.json", "r") as jsonFile:
-        user_data = json.loads(jsonFile.read())
+    browser_type = test_data["browserType"]
+    url = test_data["url"]
+    
+    browser = Browser(browser_type)
+    browser.go_to_url(url)
 
     expected_register_message = "Thank you for registering."
 
     page_object = PageObject(driver=browser.driver)
-    assert expected_register_message in page_object.register_user(user_data["registerInfo"])
+    assert expected_register_message in page_object.register_user(test_data["registerInfo"])
 
     browser.close()
